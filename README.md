fa44f869e7e7fbba8d8592e8d29d5791dcec18b2
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2023-05-21 00:07:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## License
+
+This project's code is licensed under [ARR](LICENSE). If you would like clarification or explicit permission, contact me on Discord.
---
+++ src/main/java/com/ninni/spawn/Spawn.java
@@ -0,0 +1,10 @@
+package com.ninni.spawn;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Spawn implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
