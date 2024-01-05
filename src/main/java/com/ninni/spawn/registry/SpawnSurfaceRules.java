21e0ef88e78d46baccf69e12c78e9d5c6eda9a73
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2024-01-05 01:26:30 +0100

    GRAHDNSKJADHSAKDA

--- src/main/java/com/ninni/spawn/registry/SpawnSurfaceRules.java
+++ src/main/java/com/ninni/spawn/registry/SpawnSurfaceRules.java
@@ -15,12 +15,21 @@ public class SpawnSurfaceRules {
         SurfaceRules.RuleSource ruleSource = SurfaceRules.ifTrue(SurfaceRules.stoneDepthCheck(0, false, CaveSurface.FLOOR), ALGAL_SAND);
         SurfaceRules.RuleSource ruleSource2 = SurfaceRules.ifTrue(SurfaceRules.not(SurfaceRules.waterBlockCheck(0, 0)), ruleSource);
 
-        SurfaceRules.RuleSource ruleSource3 = SurfaceRules.ifTrue(SurfaceRules.stoneDepthCheck(2, true, CaveSurface.FLOOR), SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SANDSTONE), SAND));
-        SurfaceRules.RuleSource ruleSource4 = SurfaceRules.ifTrue(SurfaceRules.not(SurfaceRules.waterStartCheck(0, 0)), ruleSource3);
+        //SurfaceRules.RuleSource ruleSource3 = SurfaceRules.ifTrue(SurfaceRules.stoneDepthCheck(-6, false, CaveSurface.FLOOR), SANDSTONE);
+        //SurfaceRules.RuleSource ruleSource4 = SurfaceRules.ifTrue(SurfaceRules.not(SurfaceRules.waterBlockCheck(0, 0)), ruleSource3);
 
-        SurfaceRules.RuleSource ruleSource5 = SurfaceRules.sequence(ruleSource2, ruleSource4);
+        //SurfaceRules.RuleSource ruleSource3 = SurfaceRules.ifTrue(SurfaceRules.stoneDepthCheck(2, true, CaveSurface.FLOOR), SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SANDSTONE), SAND));
+        //SurfaceRules.RuleSource ruleSource4 = SurfaceRules.ifTrue(SurfaceRules.not(SurfaceRules.waterStartCheck(0, 0)), ruleSource3);
 
-        SurfaceRules.RuleSource seagrassMeadow = SurfaceRules.ifTrue(SurfaceRules.isBiome(SpawnBiomes.SEAGRASS_MEADOW), ruleSource5);
+        //SurfaceRules.RuleSource ruleSource8 = SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SANDSTONE), SAND);
+        //SurfaceRules.RuleSource ruleSource5 = SurfaceRules.ifTrue(SurfaceRules.VERY_DEEP_UNDER_FLOOR, SANDSTONE);
+
+        //SurfaceRules.RuleSource ruleSource3 = SurfaceRules.ifTrue(SurfaceRules.stoneDepthCheck(4, true, CaveSurface.FLOOR), SANDSTONE);
+        //SurfaceRules.RuleSource ruleSource4 = SurfaceRules.ifTrue(SurfaceRules.waterStartCheck(-6, -1), ruleSource5);
+
+        SurfaceRules.RuleSource ruleSource6 = SurfaceRules.sequence(ruleSource2);
+
+        SurfaceRules.RuleSource seagrassMeadow = SurfaceRules.ifTrue(SurfaceRules.isBiome(SpawnBiomes.SEAGRASS_MEADOW), ruleSource6);
 
         return SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, seagrassMeadow));
     }
